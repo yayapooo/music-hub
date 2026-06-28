b7d0b88c58bafd55ebbfef69db0b05cb0d60e7bb
yayapooo <l300240033@student.ums.ac.id>
2026-06-28 17:52:33 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,25 +6,94 @@ input.addEventListener("keypress", function(e){
     }
 });
 
+window.onload = function(){
+    loadRecommendation();
+};
+
+document.addEventListener("click", function(e){
+
+    if(e.target.classList.contains("artist-card")){
+
+        input.value = e.target.innerText;
+
+        searchMusic();
+    }
+});
+
+async function loadRecommendation(){
+
+    const artists = [
+        "Taylor Swift",
+        "Ed Sheeran",
+        "Coldplay",
+        "Bruno Mars",
+        "Adele",
+        "Ariana Grande"
+    ];
+
+    let output = "";
+
+    for(let artist of artists){
+
+        const response = await fetch(
+            `https://itunes.apple.com/search?term=${artist}&entity=song&limit=1`
+        );
+
+        const data = await response.json();
+
+        const song = data.results[0];
+
+        output += `
+        <div class="card">
+
+            <img src="${song.artworkUrl100.replace(
+                "100x100",
+                "500x500"
+            )}">
+
+            <h3>${song.trackName}</h3>
+
+            <p>🎤 ${song.artistName}</p>
+
+            <p>💿 ${song.collectionName}</p>
+
+            <p>🎵 ${song.primaryGenreName}</p>
+
+            <audio controls>
+                <source src="${song.previewUrl}">
+            </audio>
+
+        </div>
+        `;
+    }
+
+    document.getElementById("recommendList").innerHTML =
+    output;
+}
+
 async function searchMusic(){
 
     const keyword = input.value;
 
     if(keyword === ""){
-        alert("Masukkan kata pencarian!");
+        alert("Please enter a song or artist.");
         return;
     }
 
-    document.getElementById("loading").style.display = "block";
+    document.getElementById("loading").style.display =
+    "block";
 
-    const url =
-    `https://itunes.apple.com/search?term=${keyword}&entity=song&limit=20`;
-
-    const response = await fetch(url);
+    const response = await fetch(
+        `https://itunes.apple.com/search?term=${keyword}&entity=song&limit=20`
+    );
 
     const data = await response.json();
 
-    document.getElementById("loading").style.display = "none";
+    document.getElementById("loading").style.display =
+    "none";
+
+    document.getElementById("resultTitle").style.display =
+    "block";
 
     let output = "";
 
@@ -60,8 +129,12 @@ async function searchMusic(){
     });
 
     if(data.results.length === 0){
-        output = "<h2>Tidak ditemukan.</h2>";
+
+        output = `
+        <h3>Song not found.</h3>
+        `;
     }
 
-    document.getElementById("musicList").innerHTML = output;
+    document.getElementById("musicList").innerHTML =
+    output;
 }
